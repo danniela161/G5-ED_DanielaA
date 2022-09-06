020d7a66362f2f5b07e608c3de7e2366d66e8516
Daniela Abitia <danielaag@cetys.edu.mx>
2022-09-05 20:22:56 -0700

    Operaciones

---
+++ HOLA MUNDO/HOLA MUNDO/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HOLA_MUNDO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HOLA PROFE Y MUNDO");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OPERACIONES1/OPERACIONES1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPERACIONES1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            int resS;
+            int resR;
+            int resM;
+            int resD;
+
+            Console.WriteLine("Ingrese un número");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Ingresa otro número");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            resS = num1 + num2;
+            resR = num1 - num2;
+            resM = num1 * num2;
+            resD = num1 / num2;
+
+            Console.WriteLine("Si los sumas será: " + resS);
+            Console.WriteLine("Si los restas será: " + resR);
+            Console.WriteLine("Si los multiplicas será: " + resM);
+            Console.WriteLine("Si los divides será: " + resD);
+            Console.WriteLine("NO INCLUYEN DECIMALES");
+
+            Console.ReadLine();
+        }
+    }
+}
